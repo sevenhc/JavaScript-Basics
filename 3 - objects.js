6259d9c3773da78e7d0a66eb979f1c0a744fa4af
SEVENHC <hasitha.chamikara@sevenhc.net>
2022-01-16 00:49:13 +0530

    objects

---
+++ 3 - objects.js
@@ -0,0 +1,35 @@
+//obect is a common data structure we use when working with JS. 
+// we can define a object as below 
+
+const myObject = {
+    FirstName: "Hasitha",
+    LastName: "Chamikara",
+    Age: 33
+}
+
+console.log(myObject);
+
+// this is not limited to just string, int ro boolean. add functions, arrays, objects and etc. 
+// just have to define them as key value pairs. 
+
+const mySecondObject = {
+    Name: "Hasitha",
+    Age: 33,
+    CoursesList: ["JavaScript", "C#", "NODE"],
+    CourseLevel: {
+        Subject: "JavaScript",
+        Level: "Intermidiete"
+    }
+}
+
+//There are multiple wasy of defining a function in a object.
+
+const myFunctionObject = {
+    add: function (a, b) { return a + b; },
+    minus(a, b) { return a - b; },
+    multiply: (a, b) => { return a * b; } // we can remove paranthesies and return keyword if it is only one line
+}
+
+console.log(myFunctionObject.add(1,2));
+console.log(myFunctionObject.minus(1,2));
+console.log(myFunctionObject.multiply(1,2));
